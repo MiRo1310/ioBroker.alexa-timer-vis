2a51c71a1bc405961b5fd05d9154317296ba24b0
MiRo1310 <michael.roling@gmx.de>
2021-12-25 10:56:11 +0100

    Erstveröffentlichung

--- admin/words.js
+++ admin/words.js
@@ -1,18 +1,41 @@
-
-// eslint-disable-next-line no-unused-vars
 /*global systemDictionary:true */
 "use strict";
 
 systemDictionary = {
 	"alexa-timer-vis adapter settings": {
-		"en": "Adapter settings for alexa-timer-vis"
+		"en": "Adapter settings for alexa-timer-vis",
+		"de": "Adaptereinstellungen für alexa-timer-vis",
+		"ru": "Настройки адаптера для alexa-timer-vis",
+		"pt": "Configurações do adaptador para alexa-timer-vis",
+		"nl": "Adapterinstellingen voor alexa-timer-vis",
+		"fr": "Paramètres d'adaptateur pour alexa-timer-vis",
+		"it": "Impostazioni dell'adattatore per alexa-timer-vis",
+		"es": "Ajustes del adaptador para alexa-timer-vis",
+		"pl": "Ustawienia adaptera dla alexa-timer-vis",
+		"zh-cn": "alexa-timer-vis的适配器设置"
 	},
 	"option1": {
 		"en": "option1",
-		"de": "Option 1"
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
 	},
 	"option2": {
 		"en": "option2",
-		"de": "Option 2"
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
 	}
 };
