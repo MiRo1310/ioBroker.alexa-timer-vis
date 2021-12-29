d16aa48d001e53f33c941b54afc8665df6c95a41
MiRo1310 <michael.roling@gmx.de>
2021-12-29 14:58:08 +0100

    Adaptation to the English language

--- main.js
+++ main.js
@@ -24,18 +24,18 @@ const timerObject = {
 	"timerActiv": {
 		"timerCount": 0, // Anzahl aktiver Timer
 		"condition": {
-			"deleteTimer": ["stopp", "stoppe", "anhalten", "lösche", "lösch"], // Vorselektion stoppen oder löschen
-			"activateTimer": ["stunde", "minute", "sekunde"], // Vorselektion hinzufügen
+			"deleteTimer": ["stopp", "stoppe", "anhalten", "lösche", "lösch","stop","delete"], // Vorselektion stoppen oder löschen
+			"activateTimer": ["stunde", "minute", "sekunde","hour","minute", "second"], // Vorselektion hinzufügen
 		},
 		"data": {
 			"interval": 1000,// Aktualisierungsinterval
-			"notNoted": ["timer", "auf", "setze", "setz"], // Wörter die nicht beachtet werden sollen
-			"stopTimer": ["stoppe", "lösche", "lösch", "stopp"], // Wörter die ein stoppen defienieren
-			"stopAll": ["alle"], // Spezielle Definition zum löschen aller Timer
-			"connecter": ["und"], // Verbindungsglied im Text, für das ein + eingesetzt werden soll
-			"hour": ["stunde", "stunden"], // Wörter für Stunden, dient als Multiplikator
-			"minute": ["minute", "minuten"], // Wörter für Minuten, dient als Multiplikator
-			"second":["sekunde", "sekunden",] // Wörter für Sekunden
+			"notNoted": ["timer", "auf", "setze", "setz","einen","set","a","for"], // Wörter die nicht beachtet werden sollen
+			"stopTimer": ["stoppe", "lösche", "lösch", "stopp","stop","delete"], // Wörter die ein stoppen defienieren
+			"stopAll": ["alle","all"], // Spezielle Definition zum löschen aller Timer
+			"connecter": ["und","and"], // Verbindungsglied im Text, für das ein + eingesetzt werden soll
+			"hour": ["stunde", "stunden","hour", "hours"], // Wörter für Stunden, dient als Multiplikator
+			"minute": ["minute", "minuten","minute","minutes"], // Wörter für Minuten, dient als Multiplikator
+			"second":["sekunde", "sekunden","second","seconds"] // Wörter für Sekunden
 		},
 		"timer": { // Liste mit Timern, zeigt den aktuellen Zustand
 			"timer1": false,
@@ -55,34 +55,62 @@ const timerObject = {
 	},
 	"zahlen": { // Zahl als Wort zu Zahl nummerisch
 		"eins": 1,
+		"one":1,
 		"eine": 1,
 		"zwei": 2,
+		"two": 2,
 		"drei": 3,
+		"three": 3,
 		"vier": 4,
+		"four": 4,
 		"fünf": 5,
+		"five": 5,
 		"sechs": 6,
+		"six": 6,
 		"sieben": 7,
+		"seven": 7,
 		"acht": 8,
+		"eight": 8,
 		"neun": 9,
+		"nine": 9,
 		"zehn": 10,
+		"ten": 10,
 		"elf": 11,
+		"eleven": 11,
 		"zwölf": 12,
+		"twelve": 12,
 		"dreizehn": 13,
+		"thirteen": 13,
 		"vierzehn": 14,
+		"fourteen": 14,
 		"fünfzehn": 15,
+		"fifteen": 15,
 		"sechszehn": 16,
+		"sixteen": 16,
 		"siebzehn": 17,
+		"seventeen": 17,
 		"achtzehn": 18,
+		"eighteen": 18,
 		"neunzehn": 19,
+		"nineteen": 19,
 		"zwanzig": 20,
+		"twenty": 20,
 		"dreißig": 30,
+		"thirty": 30,
 		"vierzig": 40,
+		"fourty": 40,
 		"fünfzig": 50,
+		"fifty": 50,
 		"sechszig": 60,
+		"sixty": 60,
 		"siebzig": 70,
+		"seventy": 70,
 		"achtzig": 80,
+		"eighty": 80,
 		"neunzig": 90,
+		"ninety": 90,
 		"hundert": 100,
+		"hundred": 100,
 	},
 	"ziffern":["0", "1", "2", "3", "4", "5", "6","7", "8", "9"],
 	"interval":{
@@ -508,7 +536,7 @@ class AlexaTimerVis extends utils.Adapter {
 					}
 				}
 				else { // Wenn nichts zutrifft kann es sich nur noch um den Namen des Timers handeln
-					name = element;
+					name += element + " ";
 				} //this.log.info("TimerString: " + timerString);
 			});
 			// Wenn das letzte Zeichen ein + ist soll es entfernt werden
