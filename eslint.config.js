3a31010839a007cabb84cd1e58109889dc92fd1a
MiRo1310 <michael.roling@gmx.de>
2024-05-26 09:40:59 +0200

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,28 @@
-module.exports = [
-    {
-        "files": ["**/*.js"],
-        "languageOptions": {
-            "ecmaVersion": 2018,
-            "globals": {
-                "es6": true,
-                "node": true,
-                "mocha": true
-            }
-        },
-        "rules": {
-            "indent": [
-                "error",
-                "tab",
-                {
-                    "SwitchCase": 1
-                }
-            ],
-            "no-console": "off",
-            "no-var": "error",
-            "no-trailing-spaces": "error",
-            "prefer-const": "error",
-            "quotes": [
-                "error",
-                "double",
-                {
-                    "avoidEscape": true,
-                    "allowTemplateLiterals": true
-                }
-            ],
-            "semi": [
-                "error",
-                "always"
-            ]
-        }
-    }  
-];
+module.exports = {
+  ignores: ["./node_modules/", "./.dev-server/"],
+  files: ["./main.js"],
+  languageOptions: {
+    ecmaVersion: 2018,
+    globals: {
+      es6: true,
+      node: true,
+      mocha: true,
+    },
+  },
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "no-console": "off",
+    "no-var": "error",
+    "no-trailing-spaces": "error",
+    "prefer-const": "error",
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: ["error", "always"],
+  },
+};
